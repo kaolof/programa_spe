25bab296db772f8ec21e48c5132ad85f704ca1ff
Kao Lo <kaochilanlo@gmail.com>
2020-10-23 00:47:42 -0400

    clase cifrado y boton -cargar preguntas-

--- Preguntas_Spe/src/Interfaz/DatosParticipante.java
+++ Preguntas_Spe/src/Interfaz/DatosParticipante.java
@@ -1,5 +1,7 @@
 package Interfaz;
 
+import java.io.File;
+import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import preguntas_spe.Participante;
@@ -7,6 +9,7 @@ import preguntas_spe.Participante;
 public class DatosParticipante extends javax.swing.JFrame {
     
     private Participante p;
+    String Ruta_archivo;
 
     public DatosParticipante() {
         initComponents();
@@ -22,6 +25,7 @@ public class DatosParticipante extends javax.swing.JFrame {
         lApellido = new javax.swing.JLabel();
         tfNombre = new javax.swing.JTextField();
         tfApellido = new javax.swing.JTextField();
+        Cargar_preguntas = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -36,12 +40,22 @@ public class DatosParticipante extends javax.swing.JFrame {
 
         lApellido.setText("Apellido:");
 
+        Cargar_preguntas.setText("Cargar preguntas");
+        Cargar_preguntas.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Cargar_preguntasActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(139, 139, 139)
+                        .addComponent(jbComenzar))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(96, 96, 96)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -52,11 +66,12 @@ public class DatosParticipante extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(lNombre)
                                 .addGap(18, 18, 18)
-                                .addComponent(tfNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(139, 139, 139)
-                        .addComponent(jbComenzar)))
+                                .addComponent(tfNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(87, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(Cargar_preguntas)
+                .addGap(110, 110, 110))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -69,7 +84,9 @@ public class DatosParticipante extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(lApellido)
                     .addComponent(tfApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
+                .addGap(26, 26, 26)
+                .addComponent(Cargar_preguntas)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 51, Short.MAX_VALUE)
                 .addComponent(jbComenzar)
                 .addGap(40, 40, 40))
         );
@@ -90,6 +107,16 @@ public class DatosParticipante extends javax.swing.JFrame {
         this.dispose();
     }//GEN-LAST:event_jbComenzarActionPerformed
 
+    private void Cargar_preguntasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Cargar_preguntasActionPerformed
+        JFileChooser jf= new JFileChooser();
+        jf.showOpenDialog(this);
+        File archivo= jf.getSelectedFile();
+        if(archivo != null){
+            Ruta_archivo=archivo.getAbsolutePath();
+        }
+        
+    }//GEN-LAST:event_Cargar_preguntasActionPerformed
+
     private void validar(JTextField tf) {
         if (tf.getText().isEmpty()) {
            JOptionPane.showMessageDialog(null, "El campo no puede esta vacío");
@@ -97,6 +124,7 @@ public class DatosParticipante extends javax.swing.JFrame {
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Cargar_preguntas;
     private javax.swing.JButton jbComenzar;
     private javax.swing.JLabel lApellido;
     private javax.swing.JLabel lNombre;
