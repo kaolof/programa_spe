e613c306bdc6a2ed933a0c74fc735c0781afe17d
Kao Lo <kaochilanlo@gmail.com>
2020-10-30 13:21:56 -0400

    interfaz mejorada

--- Preguntas_Spe/src/Interfaz/Bienvenidos.java
+++ Preguntas_Spe/src/Interfaz/Bienvenidos.java
@@ -12,43 +12,32 @@ public class Bienvenidos extends javax.swing.JFrame {
 
         jbRegistrarme = new javax.swing.JButton();
         jbAcercaDe = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jbRegistrarme.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jbRegistrarme.setText("Registrarme");
         jbRegistrarme.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbRegistrarmeActionPerformed(evt);
             }
         });
+        getContentPane().add(jbRegistrarme, new org.netbeans.lib.awtextra.AbsoluteConstraints(370, 390, 170, -1));
 
+        jbAcercaDe.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jbAcercaDe.setText("Acerca de");
         jbAcercaDe.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbAcercaDeActionPerformed(evt);
             }
         });
+        getContentPane().add(jbAcercaDe, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 340, 130, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(129, 129, 129)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jbAcercaDe, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jbRegistrarme, javax.swing.GroupLayout.PREFERRED_SIZE, 129, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(142, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(188, Short.MAX_VALUE)
-                .addComponent(jbRegistrarme)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jbAcercaDe)
-                .addGap(55, 55, 55))
-        );
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/bienvenido.PNG"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -68,6 +57,7 @@ public class Bienvenidos extends javax.swing.JFrame {
   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JButton jbAcercaDe;
     private javax.swing.JButton jbRegistrarme;
     // End of variables declaration//GEN-END:variables
--- Preguntas_Spe/src/Interfaz/DatosParticipante.java
+++ Preguntas_Spe/src/Interfaz/DatosParticipante.java
@@ -33,68 +33,45 @@ public class DatosParticipante extends javax.swing.JFrame {
         tfNombre = new javax.swing.JTextField();
         tfApellido = new javax.swing.JTextField();
         Cargar_preguntas = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jbComenzar.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         jbComenzar.setText("Comenzar ronda");
         jbComenzar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jbComenzarActionPerformed(evt);
             }
         });
+        getContentPane().add(jbComenzar, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 380, 190, -1));
 
+        lNombre.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lNombre.setForeground(new java.awt.Color(255, 255, 255));
         lNombre.setText("Nombre:");
+        getContentPane().add(lNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 190, 100, 20));
 
+        lApellido.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        lApellido.setForeground(new java.awt.Color(255, 255, 255));
         lApellido.setText("Apellido:");
+        getContentPane().add(lApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 240, 100, 20));
+        getContentPane().add(tfNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 240, 290, 20));
+        getContentPane().add(tfApellido, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 190, 290, 20));
 
+        Cargar_preguntas.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
         Cargar_preguntas.setText("Cargar preguntas");
         Cargar_preguntas.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 Cargar_preguntasActionPerformed(evt);
             }
         });
+        getContentPane().add(Cargar_preguntas, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 340, -1, -1));
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(96, 96, 96)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lApellido)
-                                .addGap(18, 18, 18)
-                                .addComponent(tfApellido))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(lNombre)
-                                .addGap(18, 18, 18)
-                                .addComponent(tfNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 158, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(133, 133, 133)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(Cargar_preguntas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jbComenzar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
-                .addContainerGap(87, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(79, 79, 79)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lNombre)
-                    .addComponent(tfNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lApellido)
-                    .addComponent(tfApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
-                .addComponent(Cargar_preguntas)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jbComenzar)
-                .addGap(40, 40, 40))
-        );
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/datos_participante.PNG"))); // NOI18N
+        jLabel2.setText("fondo");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -135,6 +112,7 @@ public class DatosParticipante extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Cargar_preguntas;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JButton jbComenzar;
     private javax.swing.JLabel lApellido;
     private javax.swing.JLabel lNombre;
